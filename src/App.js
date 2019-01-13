d96e116343c95dd99e3a2450ec79eb70a7cb5708
jamiebdavis <jamied1985@gmail.com>
2019-01-13 21:55:47 +0000

    Added AppBar, working on search component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { MuiThemeProvider } from "@material-ui/core";
+import Search from "./components/Search";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
